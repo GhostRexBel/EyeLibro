6631975e5b944c842e72f9231785ca2252d03853
Eibar Rene Ortiz Rey <94948774+reneortiz146@users.noreply.github.com>
2023-05-21 19:22:02 -0600

    Finished with back-end

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 const port = 4400;
+const servidor = "34.66.103.0";
+const nombreBD = "Books";
+const usuario = "root";
+const contrasena = "1234";
+
+const conexion = mysql.createConnection({
+  host: servidor,
+  user: usuario,
+  password: contrasena,
+  database: nombreBD,
+});
 
 app.use(
   express.urlencoded({
@@ -18,16 +30,61 @@ app.use(
 
 app.use(cors());
 
-// http://localhost:3000/transaction
-app.get("/transaction", (req, res) => {
-  res.send("se realizo un get");
+app.get("/Link", async (req, res) => {
+  try {
+    const link = await consultarLinkPorNombre(req.query.bookName);
+    return link;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
 
-// http://localhost:3000/transaction
-app.post("/transaction", (req, res) => {
-  res.send("se realizo un post");
+app.get("/LinkDownload", async (req, res) => {
+  try {
+    console.log(req.query.bookName);
+    const link = await consultarLinkDescargaPorNombre(req.query.bookName);
+    console.log(link);
+    res.json({ link: link });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, async () => {
+  try {
+    console.log(`Server is running on port ${port}`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
+
+function consultarLinkPorNombre(name) {
+  const sql = "SELECT link FROM Books.books WHERE name = ?";
+
+  return new Promise((resolve, reject) => {
+    conexion.query(sql, [name], function (error, resultados) {
+      if (error) {
+        console.error("Error al realizar la consulta:", error);
+        reject(error);
+      } else {
+        resolve(resultados[0].link);
+      }
+    });
+  });
+}
+
+function consultarLinkDescargaPorNombre(name) {
+  const sql = "SELECT linkDownload FROM Books.books WHERE name = ?";
+
+  return new Promise((resolve, reject) => {
+    conexion.query(sql, [name], function (error, resultados) {
+      if (error) {
+        console.error("Error al realizar la consulta:", error);
+        reject(error);
+      } else {
+        resolve(resultados[0].linkDownload);
+      }
+    });
+  });
+}
--- FrontEnd/Script.js
+++ FrontEnd/Script.js
@@ -1,16 +1,57 @@
-const element = document.getElementById("saveTransaction");
+const EyeTracker = document.getElementById("EyeTracker");
+const BookDownload = document.getElementById("BookDownload");
+const ViewNormal = document.getElementById("ViewNormal");
 
-element.addEventListener("click", (event) => {
-  event.preventDefault();
-  let transactionBookName = document.getElementById("transactionBook").Value;
-  let transactionBookId = document.getElementById("transactionBook").Value;
-  let transaction = {
-    bookName: transactionBookName,
-    bookId: transactionBookId,
-  };
-  let transactionjson = JSON.stringify(transaction);
-  fetch("http://localhost:3000/transaction", {
-    method: "POST",
-    body: transactionjson,
-  });
+EyeTracker.addEventListener("click", async function () {
+  let transactionBookName = this.getAttribute("data-value");
+  let url = `http://localhost:4400/Link?bookName=${transactionBookName}`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      window.open(data.link);
+    } else {
+      throw new Error("Error en la respuesta del servidor");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+BookDownload.addEventListener("click", async function () {
+  let transactionBookName = this.getAttribute("data-value");
+  let url = `http://localhost:4400/LinkDownload?bookName=${transactionBookName}`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      const link = document.createElement("a");
+      link.href = data.link;
+      link.download = transactionBookName;
+      link.click();
+    } else {
+      throw new Error("Error en la respuesta del servidor");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+ViewNormal.addEventListener("click", async function () {
+  let transactionBookName = this.getAttribute("data-value");
+  let url = `http://localhost:4400/Link?bookName=${transactionBookName}`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      window.open(data.link);
+    } else {
+      throw new Error("Error en la respuesta del servidor");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
